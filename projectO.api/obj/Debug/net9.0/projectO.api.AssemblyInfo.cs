caab01aeb5881f4475529d0e0c3673553784201e
heyitsajay <poudelajay547@gmail.com>
2025-07-28 21:35:51 +0530

    created the business logic for blogs

--- projectO.api/obj/Debug/net9.0/projectO.api.AssemblyInfo.cs
+++ projectO.api/obj/Debug/net9.0/projectO.api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("projectO.api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58ccaf4c179539c9f835225fd2ff5d6c206776c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70cb99cfc4f4648166f7d7c4ec8207d9f82c4140")]
 [assembly: System.Reflection.AssemblyProductAttribute("projectO.api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("projectO.api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
