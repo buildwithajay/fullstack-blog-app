27652b96ab4d9100e0253004e7f01de36a93f271
heyitsajay <poudelajay547@gmail.com>
2025-08-10 00:29:10 +0530

    render the blogs from backend to frontend

--- backend/Program.cs
+++ backend/Program.cs
@@ -6,11 +6,24 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowViteApp",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:5173") 
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
+
 builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
 builder.Services.AddScoped<IBlogRepository, BlogRepository>();
+
 
 var app = builder.Build();
 
@@ -18,6 +31,7 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+app.UseCors("AllowViteApp");
 app.MapControllers();
 app.UseHttpsRedirection();
 app.Run();
--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -1,12 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from '../Shared/Header'
 
 const Home = () => {
+    const [blogData, setBlogData]= useState([]);
+    const [loading, setLoading] = useState(true);
+
+     const handleClick = async (id)=>{
+      try{
+        const removeBlog = await fetch(`http://localhost:5274/blog/${id}`, {
+            method: "DELETE"
+          });
+          if(removeBlog.OK){
+            console.log("blog deleted")
+          }else{
+            console.log("blog not found")
+          }
+      }
+      catch(e){
+        console.log(e)
+      }
+    }
+    useEffect(()=>{
+      const fetchData= async()=>{
+        let api = await fetch("http://localhost:5274/blog")
+      let data =await api.json();
+      setBlogData(data);
+      setLoading(false);
+      }
+      fetchData();
+    },[handleClick])
+
+   
+    
+    if(loading){
+      return <h2>Loading ...</h2>
+    }
   return (
     <div>
-     
-      Home
-
+      {blogData.map((items)=>(
+          <div key={items.id}>
+            <h2>{items.title}</h2>
+          <p>{items.content}</p>
+          <button onClick={()=>{handleClick(items.id)}}>Delete</button>
+          </div>
+      ))}
+    
     </div>
 
 
